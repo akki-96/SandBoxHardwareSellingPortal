b53af5c48ca254da9fcc858f3e00fab0f160f287
akki-96 <75878738+akki-96@users.noreply.github.com>
2021-02-19 23:01:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hardware Selling Portal</h1>
+      <h2>This Project is Mainly Based On Selling Hardware Products Like Submersible, Water Pump, Agriculture Motor etc.</h2>
+    </div>
+  );
+}
